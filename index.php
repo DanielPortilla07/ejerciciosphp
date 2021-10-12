f6e04a4cf4ad932f2bb971eed62c211b413a140c
Daniel Portilla <danielportilla30@gmail.com>
2021-10-12 16:58:02 -0500

    segundo commit

--- index.php
+++ index.php
@@ -1,5 +1,26 @@
 <?php
 
+// Declarar 3 variables y comparar con IF si son iguales en valor (==) y en valor y tipo (===)
+// Realizar una comparacion de un valor menor que otro 
+// Ambos casos imprimir un resultado en pantalla 
+$var1 = 7;
+$var2 = 7;
+$var3 = "7";
+
+// Comparación en valor
+if ($var1 == $var2 and $var2 == $var3){
+    echo 'Comparación en valor: <br>' . $var1 . ', ' . $var2 . ' y ' . $var3 . ' son iguales en valor <br>';
+} else {
+    echo 'Comparación en valor: <br>' . $var1 . ', ' . $var2 . ' y ' . $var3 . ' no son iguales en valor <br>';
+}
+
+// Comparación en valor y tipo
+if ($var1 === $var2 and $var2 === $var3){
+    echo 'Comparación en valor y tipo: <br>' . $var1 . ', ' . $var2 . ' y ' . $var3 . ' son iguales en valor y tipo <br>';
+} else {
+    echo 'Comparación en valor y tipo: <br>' . $var1 . ', ' . $var2 . ' y ' . $var3 . ' no son iguales en valor y tipo <br>';
+}
+
 // Usuario mayor o menor de edad
 $user = "Daniel";
 $ages = 18;
